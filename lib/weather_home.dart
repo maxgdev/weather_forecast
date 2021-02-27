985bd4881d856a3823abc48bb78ac9091bd87eed
maxgdev <max@maxgdev.net>
2021-02-27 15:23:43 +0000

    Added textFieldView input widget and FutureBuilder to retrieve http data

--- lib/weather_home.dart
+++ lib/weather_home.dart
@@ -11,35 +11,63 @@ class WeatherForecast extends StatefulWidget {
 }
 
 class _WeatherForecastState extends State<WeatherForecast> {
-  
   Future<WeatherModel> forecastObject;
 
   String _cityName = "London";
+
   @override
   void initState() {
     super.initState();
     forecastObject = Network().getWeatherForecast(cityName: _cityName);
-    forecastObject.then((weather) => {
-      print(weather.city.name)
-    });
+    // forecastObject.then((weather) => {
+    //   print(weather.city.name)
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Weather Forecast App',
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          children: [
+            textFieldView(),
+            Container(
+              child: FutureBuilder<WeatherModel>(
+                future: forecastObject,
+                builder: (BuildContext context,
+                    AsyncSnapshot<WeatherModel> snapshot) {
+                  if (snapshot.hasData) {
+                    // do something
+                    return Text("Data found");
+                  } else {
+                    // do something else
+                    return Container(
+                        child: Center(
+                      child: CircularProgressIndicator(),
+                    ));
+                  }
+                },
+              ),
             ),
           ],
-        ),
-      ),
-    );
+        ));
   }
+}
+
+Widget textFieldView() {
+  return Container(
+    child: TextField(
+      decoration: InputDecoration(
+        hintText: "Enter City",
+        prefix: Icon(Icons.search),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        contentPadding: EdgeInsets.all(8),
+      ),
+      onSubmitted: (value) {
+        // ToDO
+      },
+    ),
+  );
 }
