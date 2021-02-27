5689cd452715a96f988dbf04c294f4209c2696a1
maxgdev <max@maxgdev.net>
2021-02-27 17:40:40 +0000

    Used 'intl' function to format dt field from snapshot (http data) for display.

--- lib/middle_view.dart
+++ lib/middle_view.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 import './weather_model.dart';
+import './forecast_util.dart';
 
 Widget middleView(AsyncSnapshot<WeatherModel> snapshot) {
   var forecastList = snapshot.data.list;
   var city = snapshot.data.city.name;
   var country = snapshot.data.city.country;
 
+  var formattedDate =
+      new DateTime.fromMillisecondsSinceEpoch(forecastList[0].dt);
   Container middleView = Container(
     child: Padding(
       padding: const EdgeInsets.all(14.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [Text("$city, $country",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 18,
-          color: Colors.black87,
+        children: [
+          Text(
+            "$city, $country",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              color: Colors.black87,
+            ),
           ),
-        )],
+          Text("${Util.getFormattedDate(formattedDate)}"),
+        ],
       ),
     ),
   );
