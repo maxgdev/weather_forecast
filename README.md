90d497e71c7e9d3197c3afc126defbe4b99cd916
maxgdev <max@maxgdev.net>
2021-02-27 10:16:01 +0000

    Initial commit after biolerplate code creation and cleanup

---
+++ README.md
@@ -0,0 +1,13 @@
+# weather_forecast
+
+Flutter Weather Forecast Project
+
+## Credits
+- [Packt Publishing] ()
+- https://openweathermap.org/
+- https://app.quicktype.io/
+- https://jsonformatter.curiousconcept.com/
+- http://jsonplaceholder.typicode.com/
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- [online documentation](https://flutter.dev/docs), which offers tutorials,
+
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import './weather_home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Hide the Debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Weather Forecast',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Weather Forecast'),
+    );
+  }
+}
+
