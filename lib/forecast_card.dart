a363ab310cbfa9febec3e2a6afebc2b74cd0c804
maxgdev <max@maxgdev.net>
2021-02-28 10:46:17 +0000

    Completed static mockup of forecastCard

--- lib/forecast_card.dart
+++ lib/forecast_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import './weather_model.dart';
 import './forecast_util.dart';
+import './convert_icon.dart';
+
 
 Widget forecastCard(AsyncSnapshot<WeatherModel> snapshot, int index) {
   var forecastList = snapshot.data.list;
@@ -13,10 +15,60 @@ Widget forecastCard(AsyncSnapshot<WeatherModel> snapshot, int index) {
   print("After assignment: $dayOfWeek");
   return Column(
     mainAxisAlignment: MainAxisAlignment.start,
-    crossAxisAlignment: CrossAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.center,
     children: [
-      Padding(padding: EdgeInsets.symmetric(vertical: 3)),
+      Padding(padding: EdgeInsets.only(top: 3)),
       Center(child: Text(dayOfWeek)),
+      SizedBox(height: 5),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.white,
+            // {String weatherDescription, Color color, double size}
+            child: getWeatherIcon(
+              weatherDescription: forecastList[index].weather[0].main,
+              color: Colors.amber,
+              size: 35
+              ),
+          ),
+        ],
+      ),
+      SizedBox(height: 5),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Temp: 54 ℉"),
+              Icon(Icons.thermostat_rounded , size: 16, color: Colors.black87),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Max: 59 ℉"),
+              Icon(Icons.thermostat_outlined , size: 16, color: Colors.black87),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Hum: 60 %"),
+              Icon(Icons.water_damage_outlined , size: 16, color: Colors.black87),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Wind: 5 m/h"),
+              Icon(Icons.waves_outlined , size: 16, color: Colors.black87),
+            ],
+          ),
+        ],
+      )
     ],
   );
 }
